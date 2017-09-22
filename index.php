04751aeebffb573b043cc9cd1d74f5208e6ff2d7
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-09-22 21:21:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello LINE BOT";
+$access_token = 'gubnE7WIUL1RccP5RZbHB3en8EKwUq8v6SxhsvwkzfypBP13ztGghrIdc0YEHOj5xKL75e/FtxNzwryggr1msV//b3Pk1G06ZbZC1FX+vsppw/AhcvOnIJgX5U32e8lGTqnPnesJ4c6/vJsngSSGQgdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
