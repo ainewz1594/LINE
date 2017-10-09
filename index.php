0acbe0b4ddbd4f5990b6f98a688aad2033ca6b53
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-10 00:28:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,20 @@
 
 <center> <h1>Welcome To Smart Gardent Control System</h1>
 <h2><img src="http://inhabitat.com/wp-content/blogs.dir/1/files/2015/10/Droponic-smart-kitchen-garden-7-1020x610.jpg"width="1000px" height="400px"></h2>
-<p>Test</p>
+<p>หน้าต่างแสดงผล</p>
 
-<button onClick = "window.location = 'https://smart-kmutnb.herokuapp.com/'"> ข้อความ </button>
+<button onClick = "window.location = 'https://smart-kmutnb.herokuapp.com/'"> Refresh </button>
+<p><p>
+ <p align = "left"><img src="" hspace=30> จุดตรวจวัดที่ 1
+ <p align = "left"> <INPUT  TYPE=PASSWORD  SIZE=n1  NAME=fieldname  MAXLENGTH=n2> <tr> ค่าความชื้นในดิน </tr>
+<p><p>
+ <p align = "left"><INPUT  TYPE=PASSWORD  SIZE=n1  NAME=fieldname  MAXLENGTH=n2> <tr> ความสว่าง </tr>
+<p><p>
+ <p align = "left"><INPUT  TYPE=PASSWORD  SIZE=n1  NAME=fieldname  MAXLENGTH=n2> <tr> อุณหภูมิ </tr>
+ <p></p>
+ <p align = "left"><img src="" hspace=20> สถานะการทำงาน
+  <p></p>
+ <p align = "left"><img src="" hspace=35><button style = “#CC0066” hspace=30 > ON/OFF </button>
 
 </body>
 </html>
