07405788128ab8c5bb59490d1473df94104b5d36
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-09 23:45:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 
 <h1>Welcome To Smart Gardent</h1>
- <h2><img src="http://inhabitat.com/wp-content/blogs.dir/1/files/2015/10/Droponic-smart-kitchen-garden-7-1020x610.jpg"></h2>
+ <h2><img src="http://inhabitat.com/wp-content/blogs.dir/1/files/2015/10/Droponic-smart-kitchen-garden-7-1020x610.jpg"width="1000px" height="400px"> ></h2>
 <p></p>
 
 </body>
