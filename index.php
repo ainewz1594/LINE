dc422b66a4b549dd34e69a2bd018ac3c5e091bf8
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-09 23:36:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,12 @@
-<?php
-$access_token = 'gubnE7WIUL1RccP5RZbHB3en8EKwUq8v6SxhsvwkzfypBP13ztGghrIdc0YEHOj5xKL75e/FtxNzwryggr1msV//b3Pk1G06ZbZC1FX+vsppw/AhcvOnIJgX5U32e8lGTqnPnesJ4c6/vJsngSSGQgdB04t89/1O/w1cDnyilFU=';
+<!DOCTYPE html>
+<html>
+<head>
+<title>SmartGardent</title>
+</head>
+<body>
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+<h1>Welcome To Smart Gardent</h1>
+<p></p>
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
-}
-<img src=”https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTof5IXVCuP3X76BMYQH6OaJey2vqrVUJnKetwc3WaOdgQ1xGHSrg”>
-echo "Kmutnb-SmartGardent Test";
-
+</body>
+</html>
