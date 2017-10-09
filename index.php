5c4fba1222309157c134be72899aa28ac9536e5a
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-09 23:54:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <h2><img src="http://inhabitat.com/wp-content/blogs.dir/1/files/2015/10/Droponic-smart-kitchen-garden-7-1020x610.jpg"width="1000px" height="400px"></h2>
 <p>Test</p>
 
-<button onClick = “window.location.reload()”> Refresh </button>
+<button onClick = "window.location = 'https://smart-kmutnb.herokuapp.com/'"> ข้อความ </button>
 
 </body>
 </html>
