d8b3e0a1f5c014fc98fac1a19c7b7d08413e42c7
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-12 00:36:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 
 <center> <h1>Welcome To Smart Gardent Control System</h1>
-<h2><img src="http://inhabitat.com/wp-content/blogs.dir/1/files/2015/10/Droponic-smart-kitchen-garden-7-1020x610.jpg"width="1000px" height="400px"></h2>
+<h2><img src="http://inhabitat.com/wp-content/blogs.dir/1/files/2015/10/Droponic-smart-kitchen-garden-7-1020x610.jpg"</h2>
 <p>หน้าต่างแสดงผล</p>
 
 <button onClick = "window.location = 'https://smart-kmutnb.herokuapp.com/'"> Refresh </button>
