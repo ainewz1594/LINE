04be3ddb1116a01c74d41cdd1fbf930bba6bf6fd
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-13 00:01:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<TITLE>Smart Gardent</TITLE>
+<META NAME="Generator" CONTENT="EditPlus">
+<META NAME="Author" CONTENT="">
+<META NAME="Keywords" CONTENT="">
+<META NAME="Description" CONTENT="">
+</head>
+<style type="text/css">
+  body { background-color: #ffffff; }
+</style>
+<body>
+
+
+
+<center> <h1>Welcome To Smart Gardent Control System</h1>
+<h2><img src="http://www.vesternet.com/media/wysiwyg/GreenIQ/GreenIQ_system.jpg"></h2>
+<p>หน้าต่างแสดงผล</p>
+<button onClick = "window.location = 'https://smart-kmutnb.herokuapp.com/'"> Refresh </button>
+<p> <p>
+<table align="center">
+<tr algin="center">
+ <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=temp_sapa&type=line"></iframe></td>
+  <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity_kasarb&type=line"></iframe></td>
+  </tr>
+ </table>
+<p><p>
+
+ <p align = "center"><img src="" hspace=20> สถานะการทำงาน
+  <p></p>
+<center><input type="label" name="gender" id="On/off" value="                On/Off" ><br>
+
+ <?php
 
  $temp = file_get_contents('https://api.thingspeak.com/channels/345345/fields/1/last.txt');
 $humidity = file_get_contents('https://api.thingspeak.com/channels/345345/fields/2/last.txt');
