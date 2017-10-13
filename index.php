e197aa26bebdcc359a7f0daee5b9c89833f10880
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-14 01:48:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $now = new DateTime();
  $temp = file_get_contents('https://api.thingspeak.com/channels/345345/fields/1/last.txt');
 $humidity = file_get_contents('https://api.thingspeak.com/channels/345345/fields/2/last.txt');
 
- echo "temp is = ".$temp."<br>";
+ <FONT SIZE=5>echo "temp is = ".$temp."<br>";</FONT>
  echo "humidity is = ".$humidity."<br>";
  echo "Time Update = ".$datenow."<br>";
 ?>
