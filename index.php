a8834a9d0d5e48b661c2456bd7cb76e09025c4d6
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-14 01:47:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,9 @@
 <body>
 <center> <h1>Welcome To Smart Gardent Control System</h1>
 <h2><img src="http://www.vesternet.com/media/wysiwyg/GreenIQ/GreenIQ_system.jpg"></h2>
-<p><font color=##3366CC>หน้าต่างแสดงผล</p>
-<button onClick = "window.location = 'https://smart-kmutnb.herokuapp.com/'"> Refresh </button>
+<font color=#000080><FONT SIZE=6>หน้าต่างแสดงผล</FONT></FONT><br>
+<br>
+<button onClick = "window.location = 'https://smart-kmutnb.herokuapp.com/'"> Refresh </button><br>
 <table align="center">
 <tr algin="center">
  <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=temp_C&type=line"></iframe></td>
