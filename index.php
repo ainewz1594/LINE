dd25f6ae0bafd2f199131f62eacb8c17d3adac56
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-14 00:09:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,5 +35,5 @@ $humidity = file_get_contents('https://api.thingspeak.com/channels/345345/fields
  echo "temp is = ".$temp."<br>";
  echo "humidity is = ".$humidity."<br>";
  echo "Time Update = ".$datenow;
-
+ echo " ";
 ?>
