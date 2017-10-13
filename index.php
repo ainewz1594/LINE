60359fe73a78c13304a89f2414b5a036d5efc687
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-14 01:57:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<TITLE>Smart Gardent</TITLE>
+<TITLE>SmartGardent Kmutnb</TITLE>
 <META NAME="Generator" CONTENT="EditPlus">
 <META NAME="Author" CONTENT="">
 <META NAME="Keywords" CONTENT="">
@@ -11,11 +11,12 @@
   body { background-color: #ffffff; }
 </style>
 <body>
-<center> <h1>Welcome To Smart Gardent Control System</h1>
+<center><h1><font color=#330066>Welcome To Smart Gardent Control System</FONT></h1>
 <h2><img src="http://www.vesternet.com/media/wysiwyg/GreenIQ/GreenIQ_system.jpg"></h2>
 <font color=#000080><FONT SIZE=6>หน้าต่างแสดงผล</FONT></FONT><br>
 <br>
 <button onClick = "window.location = 'https://smart-kmutnb.herokuapp.com/'"> Refresh </button><br>
+  <p> </p>
 <table align="center">
 <tr algin="center">
  <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=temp_C&type=line"></iframe></td>
