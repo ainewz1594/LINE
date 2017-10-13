8174524b0d35328136ef2f10ac6e07da0eb3f7fa
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-14 01:00:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <table align="center">
 <tr algin="center">
  <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=temp_C&type=line"></iframe></td>
-  <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity_%&type=line"></iframe></td>
+  <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity_percen&type=line"></iframe></td>
   </tr>
  </table>
  <p align = "center">สถานะการทำงาน<p></p>
