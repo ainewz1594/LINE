256f40aec9dfaf8b78cd7fd614b9d738544bb229
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-14 00:59:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
 <button onClick = "window.location = 'https://smart-kmutnb.herokuapp.com/'"> Refresh </button>
 <table align="center">
 <tr algin="center">
- <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=temp&type=line"></iframe></td>
-  <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line"></iframe></td>
+ <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=temp_C&type=line"></iframe></td>
+  <td align="center"><iframe width="450" height="300" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/345345/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity_%&type=line"></iframe></td>
   </tr>
  </table>
  <p align = "center">สถานะการทำงาน<p></p>
