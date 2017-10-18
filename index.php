556695b3355cfc80f48135ae2e589fbc8f7b9dfa
ainewz1594 <32198935+ainewz1594@users.noreply.github.com>
2017-10-18 21:42:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -50,5 +50,11 @@ $humidity = file_get_contents('https://api.thingspeak.com/channels/345345/fields
  echo "temp is = ".$temp."<br>";
  echo "humidity is = ".$humidity."<br>";
  echo "Time Update = ".$datenow."<br>";
+  
+function useif($temp){
+	if($temp < 30) echo "temp is = Low";
+	if($temp >= 30 && $temp < 40) echo "temp is = High";
+}
+  
 ?>
   
